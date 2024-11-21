819b49f950b16ace8efcc0adf2f280c500599d4d
Jakub Wyszynski <wyszynski.jakub03@gmail.com>
2024-11-21 22:07:42 +0100

    calendar

--- training-planning/src/app/app.component.ts
+++ training-planning/src/app/app.component.ts
@@ -1,12 +1,31 @@
+// import { Component } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+// import { StudentsComponent } from './students/students.component';
+// import { AddStudentComponent } from './add-student/add-student.component';
+
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [RouterOutlet, StudentsComponent, AddStudentComponent],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css'
+// })
+// export class AppComponent {
+//   title = 'student-app';
+// }
+
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CalendarOptions } from '@fullcalendar/core'; // useful for typechecking
+import dayGridPlugin from '@fullcalendar/daygrid';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'training-planning';
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    plugins: [dayGridPlugin]
+  };
 }
--- training-planning/src/main.ts
+++ training-planning/src/main.ts
@@ -1,6 +1,19 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { appConfig } from './app/app.config';
+// import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+// bootstrapApplication(AppComponent, appConfig)
+//   .catch((err) => console.error(err));
+
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, Routes } from '@angular/router';
+import { CalendarComponent } from './app/calendar/calendar.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'calendar', pathMatch: 'full' },
+  { path: 'calendar', component: CalendarComponent },
+];
+
+bootstrapApplication(CalendarComponent, {
+  providers: [provideRouter(routes)],
+}).catch((err) => console.error(err));
